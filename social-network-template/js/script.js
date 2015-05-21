e8f93d5f9f35a5e0413e2289bdcd71ac3397bd16
Jim Shaw <jimxshaw@gmail.com>
2015-05-20 22:26:09 -0500

    finished photos.html

--- social-network-template/js/script.js
+++ social-network-template/js/script.js
@@ -1,4 +1,8 @@
 $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
-}); 
+});
+
+$(function () {
+  $('[data-hover="tooltip"]').tooltip()
+}) 
